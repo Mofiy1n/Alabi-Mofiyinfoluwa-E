49c43c952c82d1a567367e1ab7a16790db44e7df
Mofiy1n <alabifiyin7@gmail.com>
2026-01-21 07:35:56 +0100

    Alabi mofiyinfoluwa.py

--- Alabi mofiyinfoluwa.py
+++ Alabi mofiyinfoluwa.py
@@ -1,3 +1,4 @@
+# Binary Tree Node
 class Node:
     def __init__(self, data):
         self.data = data
@@ -5,49 +6,60 @@ class Node:
         self.right = None
 
 
-def inorder(node):
-    if node:
-        inorder(node.left)
-        print(node.data, end=" ")
-        inorder(node.right)
+# Inorder Traversal
+def inorder(root):
+    if root is not None:
+        inorder(root.left)
+        print(root.data, end=" ")
+        inorder(root.right)
 
 
-def preorder(node):
-    if node:
-        print(node.data, end=" ")
-        preorder(node.left)
-        preorder(node.right)
+# Preorder Traversal
+def preorder(root):
+    if root is not None:
+        print(root.data, end=" ")
+        preorder(root.left)
+        preorder(root.right)
 
 
-def postorder(node):
-    if node:
-        postorder(node.left)
-        postorder(node.right)
-        print(node.data, end=" ")
+# Postorder Traversal
+def postorder(root):
+    if root is not None:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data, end=" ")
 
 
-def height(node):
-    if node is None:
+# Function to calculate height of the tree
+def height(root):
+    if root is None:
         return -1
-    left_height = height(node.left)
-    right_height = height(node.right)
-    return max(left_height, right_height) + 1
+    return max(height(root.left), height(root.right)) + 1
 
 
+# Creating the binary tree using the given numbers
+root = Node(23)
 
-root = Node(1)
-root.left = Node(2)
-root.right = Node(3)
-root.left.left = Node(4)
-root.left.right = Node(5)
+root.left = Node(5)
+root.right = Node(18)
 
-print("Inorder traversal:")
+root.left.left = Node(70)
+root.left.right = Node(10)
+
+root.left.right.left = Node(8)
+
+root.right.left = Node(15)
+root.right.left.right = Node(12)
+
+
+# Display results
+print("Inorder Traversal:")
 inorder(root)
 
-print("\nPreorder traversal:")
+print("\nPreorder Traversal:")
 preorder(root)
 
-print("\nPostorder traversal:")
+print("\nPostorder Traversal:")
 postorder(root)
 
 print("\nHeight of the tree:", height(root))
