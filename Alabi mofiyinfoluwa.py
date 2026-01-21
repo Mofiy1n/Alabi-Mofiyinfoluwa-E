d3263a1d9b9fd052a3792a649211c97cc9a0633a
Mofiy1n <alabifiyin7@gmail.com>
2026-01-21 06:06:49 +0100

    Add files via upload

---
+++ Alabi mofiyinfoluwa.py
@@ -0,0 +1,53 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def inorder(node):
+    if node:
+        inorder(node.left)
+        print(node.data, end=" ")
+        inorder(node.right)
+
+
+def preorder(node):
+    if node:
+        print(node.data, end=" ")
+        preorder(node.left)
+        preorder(node.right)
+
+
+def postorder(node):
+    if node:
+        postorder(node.left)
+        postorder(node.right)
+        print(node.data, end=" ")
+
+
+def height(node):
+    if node is None:
+        return -1
+    left_height = height(node.left)
+    right_height = height(node.right)
+    return max(left_height, right_height) + 1
+
+
+# Creating the binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print("Inorder traversal:")
+inorder(root)
+
+print("\nPreorder traversal:")
+preorder(root)
+
+print("\nPostorder traversal:")
+postorder(root)
+
+print("\nHeight of the tree:", height(root))
